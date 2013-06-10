24950b5a514add91d7faf6734956cdc64481977d
rakesh <ntf@ntf-System-Product-Name.(none)>
2013-06-10 12:52:02 +0530

    useing google maps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < CouchRest::Model::Base
+	acts_as_gmappable :position => :location
+	field :location, :type => Array
+		def gmaps4rails_address
+			#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+			"#{self.street}, #{self.city}, #{self.country}" 
+  		end
+
   property :first_name,String
   property :last_name,String   
   property :nick_name,String  
@@ -8,4 +15,6 @@ class User < CouchRest::Model::Base
   property :yahoo,String
   property :gabeer,String
   property :about_you,String
+  property :address
+  validates_uniqueness_of :first_name,:email,:yahoo
 end
