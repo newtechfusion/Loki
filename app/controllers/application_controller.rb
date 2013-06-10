1e06a4084b87b583d08cf44c6b3c151a6d032e6e
Arvind <=>
2013-06-10 19:15:57 +0530

    give the option of map in all the location view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+def index
+    @json = Location.all.to_gmaps4rails do |device, marker|
+    marker.picture({
+     :picture => "#{device.pin.image_url unless device.pin.nil?}", # up to you to pass the proper parameters in the url, I guess with a method from device
+     :width   => 32,
+     :height  => 32
+   })
+end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @locations }
+    end
+
+end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,13 @@ class LocationsController < ApplicationController
   # GET /locations.json
   def index
     @locations = Location.all
-     @json = Location.all.to_gmaps4rails
+       @json = Location.all.to_gmaps4rails do |device, marker|
+    marker.picture({
+     :picture => "#{device.pin.image_url unless device.pin.nil?}", # up to you to pass the proper parameters in the url, I guess with a method from device
+     :width   => 32,
+     :height  => 32
+   })
+end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @locations }
@@ -14,7 +20,13 @@ class LocationsController < ApplicationController
   # GET /locations/1.json
   def show
     @location = Location.find(params[:id])
-
+       @json = Location.all.to_gmaps4rails do |device, marker|
+    marker.picture({
+     :picture => "#{device.pin.image_url unless device.pin.nil?}", # up to you to pass the proper parameters in the url, I guess with a method from device
+     :width   => 32,
+     :height  => 32
+   })
+end
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @location }
@@ -42,6 +54,13 @@ end
   # GET /locations/1/edit
   def edit
     @location = Location.find(params[:id])
+    @json = Location.all.to_gmaps4rails do |device, marker|
+    marker.picture({
+     :picture => "#{device.pin.image_url unless device.pin.nil?}", # up to you to pass the proper parameters in the url, I guess with a method from device
+     :width   => 32,
+     :height  => 32
+   })
+end
   end
 
   # POST /locations
