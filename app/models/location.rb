958b8a7a482e4e173af33be1b128c1bc63d4e22b
Arvind <=>
2013-06-11 12:18:16 +0530

    fix the address issue on map

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,10 +7,9 @@ class Location < CouchRest::Model::Base
 	property :latitude, Float
 	property :gmaps ,String
 	property :pin_id , Integer
-	acts_as_gmappable
-      def gmaps4rails_address
-          address
-      end
+	acts_as_gmappable :process_geocoding => false   # def gmaps4rails_address
+      #     address
+      # end
       def gmaps4rails_infowindow
         "<h4>#{name}</h4>" << "<h4>#{address}</h4>"
       end
