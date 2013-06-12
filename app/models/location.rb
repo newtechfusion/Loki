2908b606cb0eae44ff78f67ce92d4883fa367283
Arvind <=>
2013-06-12 20:45:28 +0530

    add some aditionaly field

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,18 +1,21 @@
 class Location < CouchRest::Model::Base
-  include Gmaps4rails::ActsAsGmappable
-    belongs_to :pin
-	property :name, String
-	property :address, String
-	property :longitude, Float
-	property :latitude, Float
-	property :gmaps ,String
-	property :pin_id , Integer
-	acts_as_gmappable :process_geocoding => false   # def gmaps4rails_address
-      #     address
+include Gmaps4rails::ActsAsGmappable
+  belongs_to :pin
+  property :title, String
+  property :address, String
+  property :description , String
+  property :longitude, Float
+  property :latitude, Float
+  property :site_link, String
+  property :gmaps ,String
+  property :pin_id , Integer
+  acts_as_gmappable :process_geocoding => false   
+        #     address
       # end
       def gmaps4rails_infowindow
-        "<h4>#{name}</h4>" << "<h4>#{address}</h4>"
+        "<h4>#{title}</h4>" << "<h4>#{address}</h4>"<< "<h5>#{description}</h5>"<< "<a href=#{site_link}>#{site_link}</a>"
       end
+
 
       def self.show_map
         Location.all.to_gmaps4rails do |device, marker|
